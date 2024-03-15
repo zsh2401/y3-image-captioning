65b3a4f5e5bf88418645b2036332a1ba48de4623
zsh2401 <zsh2401@163.com>
2024-03-15 22:49:35 +0800

    go

---
+++ server/apis/auth.py
@@ -0,0 +1,10 @@
+def register_auth_apis(app):
+    @app.route('/api/register', methods=['POST'])
+    def register():
+        json_body = request.get_json()
+        
+        # 处理表单数据...
+        
+        return jsonify({
+            "image":"abc"
+        })
