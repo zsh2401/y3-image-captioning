575f6dc3c204864e760d992ce2567ef97c682035
zsh2401 <zsh2401@163.com>
2024-03-16 15:17:35 +0800

    update environment

--- server/apis/auth.py
+++ server/apis/auth.py
@@ -1,4 +1,14 @@
 def register_auth_apis(app):
+    @app.route('/api/login', methods=['POST'])
+    def register():
+        json_body = request.get_json()
+        
+        # 处理表单数据...
+        
+        return jsonify({
+            "image":"abc"
+        })
+        
     @app.route('/api/register', methods=['POST'])
     def register():
         json_body = request.get_json()
