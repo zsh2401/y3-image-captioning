95201ecbbcb11fcaf1c7ac3507d5c0b507844351
zsh2401 <zsh2401@163.com>
2024-03-17 16:20:16 +0800

    万事俱备，只欠东风

--- server/apis/infer.py
+++ server/apis/infer.py
@@ -1,10 +1,11 @@
 import numpy as np
+import torch
 from PIL import Image
-from flask import Flask, request
+from flask import request
 
 from infer import create_infer_fn
 from server.apis.scheme import success
-from server.opts import word_map_path, model_path
+from server.opts import word_map_path, model_path, device, beam_size
 
 model = None
 
@@ -17,7 +18,7 @@ def register_infer_apis(app):
 
         global model
         if model is None:
-            model = create_infer_fn("cuda", word_map_path, model_path, 5)
+            model = create_infer_fn(device, word_map_path, model_path, beam_size)
 
         result = model(__to_tensors(images))
 
@@ -28,11 +29,29 @@ def register_infer_apis(app):
 
 
 def __to_tensors(form_files):
+    from scipy.misc import imresize
+    import torchvision.transforms as transforms
+
     result = []
     for file in form_files:
         image = Image.open(file.stream)
         # 转换图像为 NumPy 数组
-        image_np = np.array(image)
-        result.append(image_np)
+        img = np.array(image)
+
+        if len(img.shape) == 2:
+            img = img[:, :, np.newaxis]
+            img = np.concatenate([img, img, img], axis=2)
+        img = imresize(img, (256, 256))
+        img = img.transpose(2, 0, 1)
+        img = img / 255.
+        img = torch.FloatTensor(img).to(device)
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
+        transform = transforms.Compose([normalize])
+        image = transform(img)  # (3, 256, 256)
+        # Encode
+        image = image.unsqueeze(0)  # (1, 3, 256, 256)
+
+        result.append(image)
 
     return result
--- server/opts.py
+++ server/opts.py
@@ -1,3 +1,5 @@
 connection_url = 'sqlite:///db.sqlite'
 model_path = ""
 word_map_path = ""
+beam_size = 5
+device = "cuda"
